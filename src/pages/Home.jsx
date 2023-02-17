74b1af5bd842724067c36ad19a2a7d3758ae402a
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-02-17 10:47:53 +0300

    Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import Courses from "../components/Courses";
+import Call from "../components/Call";
+import Testimonials from "../components/Testimonials";
+import Why from "../components/Why";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Why />
+      <Courses />
+      <Call />
+      <Testimonials />
     </div>
   );
 }
