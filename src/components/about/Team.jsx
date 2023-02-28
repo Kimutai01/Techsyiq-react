25cccec8cc8f9df673181de41fda84da27fa1dda
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-02-28 15:54:02 +0300

    Made it responsive

--- src/components/about/Team.jsx
+++ src/components/about/Team.jsx
@@ -10,7 +10,57 @@ const Team = () => {
         eclectic, vibrant group of passionate individuals. Meet the people
         leading the way at Techsyiq.
       </p>
-      <div className="flex p-10">
+      <div className="flex md:flex-row flex-col p-10">
+        <div className="w-[100%] mr-10 shadow-md border-r-10">
+          <img src={dots} alt="" />
+          <div>
+            <h4>John Doe</h4>
+          </div>
+          <p>Founder</p>
+        </div>
+        <div className="flex items-center">
+          Quis nunc blandit est pharetra aliquam venenatis. Lectus cursus nibh
+          sed eget non lacus euismod. Sit risus, aliquet adipiscing viverra.
+          Massa auctor proin ac tempor maecenas. Congue libero, dolor volutpat
+          imperdiet. Mattis mauris erat pulvinar massa a diam tempus cras lacus.
+          Enim duis tempor, et tellus quis egestas tristique pellentesque
+          elementum. Felis praesent consectetur a, a viverra. In sed aenean
+          luctus commodo at augue arcu. Interdum dolor, semper volutpat et enim.
+          Quisque rhoncus at id scelerisque sit. Pulvinar lacus diam risus
+          dignissim ipsum pretium vulputate feugiat mauris. Odio fames volutpat
+          massa est magna molestie. In tortor vitae nunc, adipiscing justo,
+          eget. Dui pellentesque massa blandit in semper. Nunc ultricies
+          faucibus et nec. Nam euismod sodales rhoncus nam sit. Tempor, leo elit
+          hendrerit amet, egestas.
+        </div>
+      </div>
+
+      <div className="flex md:flex-row-reverse flex-col-reverse p-10">
+        <div className="w-[100%] mr-10 shadow-md border-r-10">
+          <img src={dots} alt="" />
+          <div>
+            <h4>John Doe</h4>
+          </div>
+          <p>Founder</p>
+        </div>
+        <div className="flex items-center">
+          Quis nunc blandit est pharetra aliquam venenatis. Lectus cursus nibh
+          sed eget non lacus euismod. Sit risus, aliquet adipiscing viverra.
+          Massa auctor proin ac tempor maecenas. Congue libero, dolor volutpat
+          imperdiet. Mattis mauris erat pulvinar massa a diam tempus cras lacus.
+          Enim duis tempor, et tellus quis egestas tristique pellentesque
+          elementum. Felis praesent consectetur a, a viverra. In sed aenean
+          luctus commodo at augue arcu. Interdum dolor, semper volutpat et enim.
+          Quisque rhoncus at id scelerisque sit. Pulvinar lacus diam risus
+          dignissim ipsum pretium vulputate feugiat mauris. Odio fames volutpat
+          massa est magna molestie. In tortor vitae nunc, adipiscing justo,
+          eget. Dui pellentesque massa blandit in semper. Nunc ultricies
+          faucibus et nec. Nam euismod sodales rhoncus nam sit. Tempor, leo elit
+          hendrerit amet, egestas.
+        </div>
+      </div>
+
+      <div className="flex md:flex-row flex-col p-10">
         <div className="w-[100%] mr-10 shadow-md border-r-10">
           <img src={dots} alt="" />
           <div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Hero from "../components/about/Hero";
 import Team from "../components/about/Team";
 import Navbar from "../components/Navbar";
+import Faqs from "../components/TechieMod/Faqs";
+import Footer from "../components/TechieMod/Footer";
+import LearningProcess from "../components/TechieMod/LearningProcess";
+import Tutors from "../components/TechieMod/Tutors";
 
 const About = () => {
   return (
@@ -9,6 +13,10 @@ const About = () => {
       <Navbar />
       <Hero />
       <Team />
+      <Tutors />
+      <LearningProcess />
+      <Faqs />
+      <Footer />
     </div>
   );
 };
