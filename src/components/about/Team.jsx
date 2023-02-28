9274c9e7f869f2ff891d23cb2a16abcbaba4eb33
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-02-28 09:05:00 +0300

    Added team card

---
+++ src/components/about/Team.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import dots from "../../assets/teamrect.svg";
+
+const Team = () => {
+  return (
+    <div>
+      <h1>My Team</h1>
+      <p>
+        To be the coding school our students expect us to be, it takes an
+        eclectic, vibrant group of passionate individuals. Meet the people
+        leading the way at Techsyiq.
+      </p>
+      <div className="flex p-10">
+        <div className="w-[100%] mr-10 shadow-md border-r-10">
+          <img src={dots} alt="" />
+          <div>
+            <h4>John Doe</h4>
+          </div>
+          <p>Founder</p>
+        </div>
+        <div className="flex items-center">
+          Quis nunc blandit est pharetra aliquam venenatis. Lectus cursus nibh
+          sed eget non lacus euismod. Sit risus, aliquet adipiscing viverra.
+          Massa auctor proin ac tempor maecenas. Congue libero, dolor volutpat
+          imperdiet. Mattis mauris erat pulvinar massa a diam tempus cras lacus.
+          Enim duis tempor, et tellus quis egestas tristique pellentesque
+          elementum. Felis praesent consectetur a, a viverra. In sed aenean
+          luctus commodo at augue arcu. Interdum dolor, semper volutpat et enim.
+          Quisque rhoncus at id scelerisque sit. Pulvinar lacus diam risus
+          dignissim ipsum pretium vulputate feugiat mauris. Odio fames volutpat
+          massa est magna molestie. In tortor vitae nunc, adipiscing justo,
+          eget. Dui pellentesque massa blandit in semper. Nunc ultricies
+          faucibus et nec. Nam euismod sodales rhoncus nam sit. Tempor, leo elit
+          hendrerit amet, egestas.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Team;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Hero from "../components/about/Hero";
+import Team from "../components/about/Team";
 import Navbar from "../components/Navbar";
 
 const About = () => {
@@ -7,6 +8,7 @@ const About = () => {
     <div>
       <Navbar />
       <Hero />
+      <Team />
     </div>
   );
 };
